85ac76d31b01a4d7e3d9ec304172b226895497f9
Daniel Ebbert <daniel@ebbert.nl>
2017-11-20 11:27:42 +0100

    Initial version of the opencastR package with function to query the base api

---
+++ R/base.R
@@ -0,0 +1,111 @@
+#' Request key characteristics
+#'
+#' Returns key characteristics of the API such as the server name and the default version.
+#'
+#' @param oc_con A vector consiting of the elements: Opencast URL, user and password.
+#'
+#' @export
+#' @md
+
+oc_base <- function(oc_con) {
+  userpwd <- paste(oc_con[2], ":", oc_con[3], sep = "")
+  endpoint <- "/api/"
+  response <- jsonlite::fromJSON(RCurl::getURL(paste(oc_con[1], endpoint, sep = ""), userpwd=userpwd, httpauth = 1L))
+  return(response)
+}
+
+#' Request information about the current organization
+#'
+#' Returns the current organization.
+#'
+#' @param oc_con A vector consiting of the elements: Opencast URL, user and password.
+#'
+#' @export
+#' @md
+
+oc_organization <- function(oc_con) {
+  userpwd <- paste(oc_con[2], ":", oc_con[3], sep = "")
+  endpoint <- "/api/info/organization"
+  response <- jsonlite::fromJSON(RCurl::getURL(paste(oc_con[1], endpoint, sep = ""), userpwd=userpwd, httpauth = 1L))
+  return(response)
+}
+
+#' Request information about the current organization's properties
+#'
+#' Returns the current organization's properties.
+#'
+#' @param oc_con A vector consiting of the elements: Opencast URL, user and password.
+#'
+#' @export
+#' @md
+
+oc_organization_properties <- function(oc_con) {
+  userpwd <- paste(oc_con[2], ":", oc_con[3], sep = "")
+  endpoint <- "/api/info/organization/properties"
+  response <- jsonlite::fromJSON(RCurl::getURL(paste(oc_con[1], endpoint, sep = ""), userpwd=userpwd, httpauth = 1L))
+  return(response)
+}
+
+#' Request information on the logged in user
+#'
+#' Returns information on the logged in user.
+#'
+#' @param oc_con A vector consiting of the elements: Opencast URL, user and password.
+#'
+#' @export
+#' @md
+
+oc_user <- function(oc_con) {
+  userpwd <- paste(oc_con[2], ":", oc_con[3], sep = "")
+  endpoint <- "/api/info/me"
+  response <- jsonlite::fromJSON(RCurl::getURL(paste(oc_con[1], endpoint, sep = ""), userpwd=userpwd, httpauth = 1L))
+  return(response)
+}
+
+#' Request the roles of the logged in user
+#'
+#' Returns current user's roles.
+#'
+#' @param oc_con A vector consiting of the elements: Opencast URL, user and password.
+#'
+#' @export
+#' @md
+
+oc_user_roles <- function(oc_con) {
+  userpwd <- paste(oc_con[2], ":", oc_con[3], sep = "")
+  endpoint <- "/api/info/me/roles"
+  response <- jsonlite::fromJSON(RCurl::getURL(paste(oc_con[1], endpoint, sep = ""), userpwd=userpwd, httpauth = 1L))
+  return(response)
+}
+
+#' Request the available version as well as the default version
+#'
+#' Returns a list of available version as well as the default version.
+#'
+#' @param oc_con A vector consiting of the elements: Opencast URL, user and password.
+#'
+#' @export
+#' @md
+
+oc_api_version <- function(oc_con) {
+  userpwd <- paste(oc_con[2], ":", oc_con[3], sep = "")
+  endpoint <- "/api/version"
+  response <- jsonlite::fromJSON(RCurl::getURL(paste(oc_con[1], endpoint, sep = ""), userpwd=userpwd, httpauth = 1L))
+  return(response)
+}
+
+#' Request the default version
+#'
+#' Returns the default version.
+#'
+#' @param oc_con A vector consiting of the elements: Opencast URL, user and password.
+#'
+#' @export
+#' @md
+
+oc_api_version_default <- function(oc_con) {
+  userpwd <- paste(oc_con[2], ":", oc_con[3], sep = "")
+  endpoint <- "/api/version/default"
+  response <- jsonlite::fromJSON(RCurl::getURL(paste(oc_con[1], endpoint, sep = ""), userpwd=userpwd, httpauth = 1L))
+  return(response)
+}
