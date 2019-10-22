50e97b8693fc5d03f3ec302640ae0e0e9b7117e5
Daniel Ebbert <daniel@ebbert.nl>
2019-10-22 13:55:50 +0200

    added functions for agents

--- R/oc_package_query.R
+++ R/oc_package_query.R
@@ -4,44 +4,30 @@
 #'
 #' @return The response of the api call.
 #' @param url The URL for the api call.
+#' @param query A list of query parameters.
 #' @import httr
 #' @importFrom jsonlite fromJSON
 #' @export
 #' @examples
 #' oc_package_query()
-oc_package_query <- function(url = "https://legacy.opencast.org/api/") {
-  # Save api call to variable
-  resp <-
-    GET(url,
-        authenticate(oc_username(), oc_password()),
-        oc_package_useragent())
+oc_package_query <-
+  function(url = "https://legacy.opencast.org/api/", query = NULL) {
+    # Save api call to variable
+    resp <-
+      GET(url,
+          query = query,
+          authenticate(oc_username(), oc_password()),
+          oc_package_useragent())
 
-  # Check if api returned json, otherwise return an error
-  if (http_type(resp) != "application/json") {
-    stop("API did not return json", call. = FALSE)
+    # Parse the response
+    parsed <-
+      fromJSON(content(resp, "text", encoding = "UTF-8"), simplifyVector = FALSE)
+
+    # Structure the return
+    structure(list(
+      content = parsed,
+      url = url,
+      response = resp
+    ),
+    class = "opencast_api")
   }
-
-  # Parse the response
-  parsed <-
-    fromJSON(content(resp, "text", encoding = "UTF-8"), simplifyVector = FALSE)
-
-  if (http_error(resp)) {
-    stop(
-      sprintf(
-        "Opencast API request failed [%s]\n%s\n<%s>",
-        status_code(resp),
-        parsed$message,
-        parsed$documentation_url
-      ),
-      call. = FALSE
-    )
-  }
-
-  # Structure the return
-  structure(list(
-    content = parsed,
-    url = url,
-    response = resp
-  ),
-  class = "opencast_api")
-}
